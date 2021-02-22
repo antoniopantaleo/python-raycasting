85f902dc557912a2996c16e956e4292cc7872b69
Antonio Pantaleo <46167308+antoniopantaleo@users.noreply.github.com>
2021-02-22 19:27:00 +0100

    funziona, ma va normalizzato

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import pygame
+from math import sqrt
 
 
 class Scene:
@@ -8,11 +9,12 @@ class Scene:
         self.screen = pygame.display.set_mode((size, size))
         self.walls = []
         self.__add_walls()
-        self.ray = Ray(self.screen, (160, 250), (320, 250))
+        self.ray = Ray(self.screen, (250, 250), (0, -100))
         pygame.init()
 
     def __add_walls(self):
-        self.walls.append(Wall(self.screen, (400, 100), (400, 400)))
+        #self.walls.append(Wall(self.screen, (300, 100), (400, 100)))
+        pass
 
     def draw(self):
         RUNNING = True
@@ -21,9 +23,7 @@ class Scene:
             for event in pygame.event.get():
                 if event == pygame.QUIT:
                     RUNNING = False
-            for wall in self.walls:
-                wall.draw()
-                print(f"** DEBUG: Hit result {self.ray.intersect(wall)}")
+            self.ray.lookAt(pygame.mouse.get_pos())
             self.ray.draw()
             pygame.display.update()
 
@@ -52,12 +52,22 @@ class Ray:
         self.screen = screen
         self.x1 = pos[0]
         self.y1 = pos[1]
-        self.x2 = dir[0]
-        self.y2 = dir[1]
+        self.x2 = self.x1 + dir[0]
+        self.y2 = self.y1 + dir[1]
 
     def draw(self):
         pygame.draw.line(self.screen, (255, 255, 255),
                          (self.x1, self.y1), (self.x2, self.y2))
+
+    def lookAt(self, pos):
+        x = pos[0]
+        y = pos[1]
+
+        V = sqrt(x**2 + y**2)
+        V = 1
+        if V > 0:
+            self.x2 = (self.x1 + (x - self.x1) / V)
+            self.y2 = (self.y1 + (y - self.y1) / V)
 
     def intersect(self, wall):
         x1 = self.x1
@@ -71,7 +81,10 @@ class Ray:
 
         denom = ((x1-x2)*(y3-y4)) - ((y1-y2)*(x3-x4))
 
-        t = (((x1-x3)*(y3-y4))-((y1-y3)*(x3-x4)))/denom
-        u = (((x2-x1)*(y1-y3))-((y2-y1)*(x1-x3)))/denom
+        if denom == 0:
+            return False
+
+        u = (((x1-x3)*(y3-y4))-((y1-y3)*(x3-x4)))/denom
+        t = (((x2-x1)*(y1-y3))-((y2-y1)*(x1-x3)))/denom
 
         return t >= 0 and t <= 1 and u >= 0
